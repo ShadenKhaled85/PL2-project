24323b9a50ae233d433f5cc8582463e4f7d94720
eyadelfar <eyadamen588@gmail.com>
2021-12-28 22:46:45 +0200

    Added

---
+++ PL project/Manager.java
@@ -0,0 +1,30 @@
+public class Manager extends Staff
+{
+    private static final double MINIMUM_RATE = 100.0;
+    
+    public Manager()
+    {
+        super();
+    }
+    public Manager( int newID, String newLastName, String newFirstName, String newPassward)
+    {
+        super(newID, newLastName, newFirstName, newPassward);
+        wageRate = MINIMUM_RATE;
+    }
+    
+    public void setWageRate(double newRate)
+    {
+        if(wageRate < MINIMUM_RATE)
+            newRate = MINIMUM_RATE;
+        wageRate = newRate;
+    }
+    
+
+    public double culculateWages()
+    {
+        if(getWorkState() != WORKSTATE_FINISH)
+            return 0;
+        
+        return this.wageRate;
+    }
+}
